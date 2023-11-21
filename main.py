87f31cbf3bec40580aaeb35ee2df45240c2e9db8
ze1den1 <135832640+ze1den1@users.noreply.github.com>
2023-11-21 20:19:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    print('Make Python great again!')
 
 
 if __name__ == '__main__':
